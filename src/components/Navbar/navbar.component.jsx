347a9b6b5d4199557a9154c0531f6abc2de28c95
AndrewChacon <andrew.b.chacon@gmail.com>
2022-04-14 12:16:24 -0400

    idk wtf

--- src/components/Navbar/navbar.component.jsx
+++ src/components/Navbar/navbar.component.jsx
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import './navbar.styles.css';
 
-export class Navbar extends Component {
-	render() {
-		return (
-			<div>
-				<h1>navbar</h1>
+function Navbar() {
+	const [toggleNav, setToggleNav] = useState(false);
+	return (
+		<div className='navbar__container'>
+			<div className='navbar__graphic'>andrew</div>
+			<div
+				className='navbar__hamburger'
+				onClick={() => setToggleNav(!toggleNav)}>
+				x
 			</div>
-		);
-	}
+			<nav className={`navbar__collapse ${toggleNav ? 'hide' : ''}`}>
+				<ul className='navbar__list'>
+					<li
+						className='navbar__item'
+						onClick={() => console.log('toggle nav')}>
+						about
+					</li>
+					<li
+						className='navbar__item'
+						onClick={() => console.log('toggle nav')}>
+						projects
+					</li>
+					<li
+						className='navbar__item active'
+						onClick={() => console.log('toggle nav')}>
+						contact
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
 }
 
 export default Navbar;
+
+/*
+icon
+
+real navbar
+hamburger
+about
+projects
+contact
+*/
